495c7efee560cd5a060d6bc9293a1051356880e2
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-03-11 22:38:02 +0400

    Update GamepadsWKC.cpp

--- Source/WebCore/platform/WKC/GamepadsWKC.cpp
+++ Source/WebCore/platform/WKC/GamepadsWKC.cpp
@@ -25,49 +25,92 @@
 
 #include "config.h"
 
-#include "Gamepads.h"
-#include "Gamepad.h"
-#include "GamepadList.h"
-
 #if ENABLE(GAMEPAD)
+
+#include "GamepadProvider.h"
+#include "GamepadProviderClient.h"
+#include "PlatformGamepad.h"
+#include <wtf/MonotonicTime.h>
+#include <wtf/NeverDestroyed.h>
+
 namespace WebCore {
 
-WKC_DEFINE_GLOBAL_PTR(Vector< RefPtr<Gamepad> >*, gGamepads, 0);
+class WKCGamepadProvider : public GamepadProvider {
+public:
+    static WKCGamepadProvider& singleton()
+    {
+        static NeverDestroyed<WKCGamepadProvider> provider;
+        return provider;
+    }
+
+    void startMonitoringGamepads(GamepadProviderClient&) override { }
+    void stopMonitoringGamepads(GamepadProviderClient&) override { }
+
+    const Vector<WeakPtr<PlatformGamepad>>& platformGamepads() override
+    {
+        return m_gamepadPtrs;
+    }
+
+    void initializeGamepads(int count)
+    {
+        m_gamepads.clear();
+        m_gamepadPtrs.clear();
+        for (int i = 0; i < count; i++) {
+            auto pad = makeUnique<PlatformGamepad>(i);
+            m_gamepadPtrs.append(pad.get());
+            m_gamepads.append(WTFMove(pad));
+        }
+    }
+
+    bool notifyGamepadEvent(int index, const String& id, MonotonicTime timestamp,
+                            int naxes, const float* axes, int nbuttons, const float* buttons)
+    {
+        if (index >= (int)m_gamepads.size())
+            return false;
+
+        auto& pad = *m_gamepads[index];
+        pad.updateId(id);
+        pad.updateTimestamp(timestamp);
+
+        if (naxes && axes) {
+            Vector<double> axisValues(naxes);
+            for (int i = 0; i < naxes; i++)
+                axisValues[i] = axes[i];
+            pad.updateAxes(axisValues);
+        }
+
+        if (nbuttons && buttons) {
+            Vector<double> buttonValues(nbuttons);
+            for (int i = 0; i < nbuttons; i++)
+                buttonValues[i] = buttons[i];
+            pad.updateButtons(buttonValues);
+        }
+
+        for (auto& client : m_clients)
+            client->platformGamepadInputActivity(EventMakesGamepadVisible::Yes);
+
+        return true;
+    }
+
+private:
+    Vector<UniquePtr<PlatformGamepad>> m_gamepads;
+    Vector<WeakPtr<PlatformGamepad>>  m_gamepadPtrs;
+};
 
 void initializeGamepads(int pads)
 {
-    if (gGamepads)
-        delete gGamepads;
-    gGamepads = new Vector< RefPtr<Gamepad> >;
-    for (int i=0; i<pads; i++) {
-        gGamepads->append(Gamepad::create());
-        gGamepads->at(i)->index(i);
-    }
+    WKCGamepadProvider::singleton().initializeGamepads(pads);
 }
 
-bool notifyGamepadEvent(int index, const WTF::String& id, long long timestamp, int naxes, const float* axes, int nbuttons, const float* buttons)
+bool notifyGamepadEvent(int index, const String& id, long long timestamp,
+                        int naxes, const float* axes, int nbuttons, const float* buttons)
 {
-    if (index>=gGamepads->size())
-        return false;
-    gGamepads->at(index)->id(id);
-    gGamepads->at(index)->timestamp(timestamp);
-    if (naxes && axes)
-        gGamepads->at(index)->axes(naxes, (float *)axes);
-    if (nbuttons && buttons)
-        gGamepads->at(index)->buttons(nbuttons, (float *)buttons);
-    return true;
+    return WKCGamepadProvider::singleton().notifyGamepadEvent(
+        index, id,
+        MonotonicTime::fromRawSeconds(timestamp / 1000.0),
+        naxes, axes, nbuttons, buttons);
 }
 
-void sampleGamepads(GamepadList* list)
-{
-    if (!gGamepads)
-        return;
+} // namespace WebCore
 
-    for(int i=0; i<gGamepads->size(); i++)
-        list->set(i, gGamepads->at(i));
-}
-
-} // namespace
-
-#endif
-
+#endif // ENABLE(GAMEPAD)
