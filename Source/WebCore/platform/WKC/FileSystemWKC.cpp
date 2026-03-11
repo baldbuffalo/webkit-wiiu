7b76e0121546e8ab21f98165cfb5937af969b97d
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-03-11 22:36:18 +0400

    Update FileSystemWKC.cpp

--- Source/WebCore/platform/WKC/FileSystemWKC.cpp
+++ Source/WebCore/platform/WKC/FileSystemWKC.cpp
@@ -4,7 +4,6 @@
  * Copyright (C) 2008 Apple Inc. All rights reserved.
  * Copyright (c) 2010-2013 ACCESS CO., LTD. All rights reserved.
  *
- *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
  * License as published by the Free Software Foundation; either
@@ -22,38 +21,16 @@
  */
 
 #include "config.h"
-#include "FileSystem.h"
-#include "AsyncFileSystem.h"
-
-#include "PlatformString.h"
-#include "CString.h"
-
+#include <wtf/FileSystem.h>
+#include <wtf/WallTime.h>
+#include <wtf/text/CString.h>
 #include "NotImplemented.h"
 
 #ifdef __WKC_IMPLICIT_INCLUDE_SYSSTAT
 #include <sys/stat.h>
 #endif
 
-namespace WebCore {
-
-String filenameToString(const char* filename)
-{
-    notImplemented();
-    return String();
-}
-
-char* filenameFromString(const String& string)
-{
-    notImplemented();
-    return NULL;
-}
-
-// Converts a string to something suitable to be displayed to the user.
-String filenameForDisplay(const String& string)
-{
-    notImplemented();
-    return string;
-}
+namespace FileSystem {
 
 bool fileExists(const String& path)
 {
@@ -76,62 +53,48 @@ bool deleteEmptyDirectory(const String& path)
     return false;
 }
 
-bool getFileSize(const String& path, long long& resultSize)
+std::optional<uint64_t> fileSize(const String& path)
 {
-    if (path.isNull() || path.isEmpty()) {
-        return false;
-    }
+    if (path.isEmpty())
+        return std::nullopt;
 
     struct stat st;
-    int err;
-
     void* fp = wkcFileFOpenPeer(WKC_FILEOPEN_USAGE_WEBCORE, path.utf8().data(), "rb");
     if (!fp)
-        return false;
+        return std::nullopt;
 
-    err = wkcFileFStatPeer(fp, &st);
+    int err = wkcFileFStatPeer(fp, &st);
     wkcFileFClosePeer(fp);
-    if (err == -1) {
-        return false;
-    }
-    resultSize = st.st_size;
+    if (err == -1)
+        return std::nullopt;
 
-    return true;
+    return static_cast<uint64_t>(st.st_size);
 }
 
-bool getFileModificationTime(const String& path, time_t& modifiedTime)
+std::optional<WallTime> fileModificationTime(const String& path)
 {
-    if (path.isNull() || path.isEmpty()) {
-        return false;
-    }
+    if (path.isEmpty())
+        return std::nullopt;
 
     struct stat st;
-    int err;
-
     void* fp = wkcFileFOpenPeer(WKC_FILEOPEN_USAGE_WEBCORE, path.utf8().data(), "rb");
     if (!fp)
-        return false;
+        return std::nullopt;
 
-    err = wkcFileFStatPeer(fp, &st);
+    int err = wkcFileFStatPeer(fp, &st);
     wkcFileFClosePeer(fp);
-    if (err == -1) {
-        return false;
-    }
+    if (err == -1)
+        return std::nullopt;
 
-    modifiedTime = st.st_mtime;
-
-    return true;
+    return WallTime::fromRawSeconds(st.st_mtime);
 }
 
 String pathByAppendingComponent(const String& path, const String& component)
 {
     char buf[MAX_PATH] = {0};
-
     int ret = wkcFilePathByAppendingComponentPeer(path.utf8().data(), component.utf8().data(), buf, MAX_PATH);
-    if (!ret) {
+    if (!ret)
         return String();
-    }
-
     return String::fromUTF8(buf);
 }
 
@@ -143,133 +106,102 @@ bool makeAllDirectories(const String& path)
 String homeDirectoryPath()
 {
     char buf[MAX_PATH] = {0};
-
     int ret = wkcFileHomeDirectoryPathPeer(buf, MAX_PATH);
-    if (!ret) {
+    if (!ret)
         return String();
-    }
-
     return String::fromUTF8(buf);
 }
 
-String pathGetFileName(const String& pathName)
+String pathFileName(const String& pathName)
 {
     char buf[MAX_PATH] = {0};
-
-    if (pathName.isEmpty() || pathName.isNull())
+    if (pathName.isEmpty())
         return String();
-
     int ret = wkcFilePathGetFileNamePeer(pathName.utf8().data(), buf, MAX_PATH);
-
-    if (!ret) {
+    if (!ret)
         return String();
-    }
-
     return String::fromUTF8(buf);
 }
 
 String directoryName(const String& path)
 {
     char buf[MAX_PATH] = {0};
-
     int ret = wkcFileDirectoryNamePeer(path.utf8().data(), buf, MAX_PATH);
-    if (!ret) {
+    if (!ret)
         return String();
-    }
-
     return String::fromUTF8(buf);
 }
 
-Vector<String> listDirectory(const String& path, const String& filter)
+Vector<String> listDirectory(const String& path)
 {
     Vector<String> entries;
     char name[MAX_PATH];
     char fullpath[MAX_PATH];
-    void *dir;
-    int ret;
 
-    dir = wkcFileOpenDirectoryPeer(WKC_FILEOPEN_USAGE_WEBCORE, path.utf8().data(), filter.utf8().data());
+    void* dir = wkcFileOpenDirectoryPeer(WKC_FILEOPEN_USAGE_WEBCORE, path.utf8().data(), "*");
     if (!dir)
         return entries;
 
     while (0 == wkcFileReadDirectoryPeer(dir, name, MAX_PATH)) {
-        ret = wkcFilePathByAppendingComponentPeer(path.utf8().data(), name, fullpath, MAX_PATH);
+        int ret = wkcFilePathByAppendingComponentPeer(path.utf8().data(), name, fullpath, MAX_PATH);
         if (!ret)
             break;
         entries.append(String::fromUTF8(fullpath));
     }
 
     wkcFileCloseDirectoryPeer(dir);
-
     return entries;
 }
 
-String openTemporaryFile(const String& prefix, PlatformFileHandle& handle)
+std::pair<String, PlatformFileHandle> openTemporaryFile(StringView prefix, StringView suffix)
 {
     char name[1024] = {0};
     void* fd = wkcFileOpenTemporaryFilePeer(prefix.utf8().data(), name, sizeof(name));
     if (!fd)
-        goto error_end;
-    handle = (PlatformFileHandle)reinterpret_cast<uintptr_t>(fd);
-    return String::fromUTF8(name);
-
-error_end:
-    handle = 0;
-    return String();
+        return { String(), invalidPlatformFileHandle };
+    return { String::fromUTF8(name), (PlatformFileHandle)reinterpret_cast<uintptr_t>(fd) };
 }
 
 PlatformFileHandle openFile(const String& path, FileOpenMode mode)
 {
-    void* fd = wkcFileFOpenPeer(WKC_FILEOPEN_USAGE_WEBCORE, path.utf8().data(), mode==OpenForRead ? "r" : "w");
-    if (!fd) {
+    const char* modeStr = (mode == FileOpenMode::Read) ? "r" : "w";
+    void* fd = wkcFileFOpenPeer(WKC_FILEOPEN_USAGE_WEBCORE, path.utf8().data(), modeStr);
+    if (!fd)
         return invalidPlatformFileHandle;
-    }
     return (PlatformFileHandle)reinterpret_cast<uintptr_t>(fd);
 }
 
 void closeFile(PlatformFileHandle& handle)
 {
-    if (handle==invalidPlatformFileHandle)
+    if (handle == invalidPlatformFileHandle)
         return;
-    void* fd = reinterpret_cast<void *>(handle);
+    void* fd = reinterpret_cast<void*>(handle);
     wkcFileFClosePeer(fd);
+    handle = invalidPlatformFileHandle;
 }
 
-int writeToFile(PlatformFileHandle handle, const char* data, int length)
+int writeToFile(PlatformFileHandle handle, const void* data, int length)
 {
-    if (handle==invalidPlatformFileHandle)
+    if (handle == invalidPlatformFileHandle)
         return 0;
-    void* fd = reinterpret_cast<void *>(handle);
-    size_t ret = wkcFileFWritePeer(data, 1, length, fd);
-    return ret;
+    void* fd = reinterpret_cast<void*>(handle);
+    return (int)wkcFileFWritePeer(data, 1, length, fd);
 }
 
-int readFromFile(PlatformFileHandle handle, char* data, int length)
+int readFromFile(PlatformFileHandle handle, void* data, int length)
 {
-    if (handle==invalidPlatformFileHandle)
+    if (handle == invalidPlatformFileHandle)
         return 0;
-    void* fd = reinterpret_cast<void *>(handle);
-    size_t ret = wkcFileFReadPeer(data, 1, length, fd);
-    return ret;
+    void* fd = reinterpret_cast<void*>(handle);
+    return (int)wkcFileFReadPeer(data, 1, length, fd);
 }
 
 long long seekFile(PlatformFileHandle handle, long long offset, FileSeekOrigin origin)
 {
-    if (handle==invalidPlatformFileHandle)
-        return 0;
-    void* fd = reinterpret_cast<void *>(handle);
-    size_t pos = wkcFileFSeekPeer(fd, offset, origin);
-    return pos;
+    if (handle == invalidPlatformFileHandle)
+        return -1;
+    void* fd = reinterpret_cast<void*>(handle);
+    return (long long)wkcFileFSeekPeer(fd, offset, (int)origin);
 }
 
-#if 0
-#if ENABLE(FILE_SYSTEM)
-bool AsyncFileSystem::crackFileSystemURL(const KURL&, Type&, String& filePath)
-{
-    notImplemented();
-    return false;
-}
-#endif // ENABLE(FILE_SYSTEM)
-#endif
-
-}
+} // namespace FileSystem
