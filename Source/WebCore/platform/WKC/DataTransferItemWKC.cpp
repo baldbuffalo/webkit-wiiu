712b5cdf36b8e1ae011b8e95b51358df47ffc917
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-03-11 22:30:29 +0400

    Update DataTransferItemWKC.cpp

--- Source/WebCore/platform/WKC/DataTransferItemWKC.cpp
+++ Source/WebCore/platform/WKC/DataTransferItemWKC.cpp
@@ -5,12 +5,12 @@
  *  modify it under the terms of the GNU Library General Public
  *  License as published by the Free Software Foundation; either
  *  version 2 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Library General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Library General Public
  *  License along with this library; if not, write to the
  *  Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
@@ -18,13 +18,9 @@
  */
 
 #include "config.h"
-
-#if ENABLE(DATA_TRANSFER_ITEMS)
 #include "DataTransferItem.h"
 #include "NotImplemented.h"
 
 namespace WebCore {
 
-} // namespace
-
-#endif
+} // namespace WebCore
