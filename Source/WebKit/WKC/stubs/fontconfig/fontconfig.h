c345b76e08633184ea55942869b3f3bb6ab1fa28
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-03-17 03:17:28 +0400

    Update fontconfig.h

--- Source/WebKit/WKC/stubs/fontconfig/fontconfig.h
+++ Source/WebKit/WKC/stubs/fontconfig/fontconfig.h
@@ -8,11 +8,12 @@ typedef int FcBool;
 #define FcFalse 0
 #define FcTrue  1
 
-typedef struct _FcPattern FcPattern;
-typedef struct _FcFontSet FcFontSet;
-typedef struct _FcConfig  FcConfig;
-typedef struct _FcCharSet FcCharSet;
-typedef struct _FcLangSet FcLangSet;
+typedef struct _FcPattern   FcPattern;
+typedef struct _FcFontSet   FcFontSet;
+typedef struct _FcConfig    FcConfig;
+typedef struct _FcCharSet   FcCharSet;
+typedef struct _FcLangSet   FcLangSet;
+typedef struct _FcObjectSet FcObjectSet;
 
 typedef enum _FcResult {
     FcResultMatch,
@@ -32,3 +33,10 @@ typedef enum _FcSetName {
     FcSetSystem = 0,
     FcSetApplication = 1
 } FcSetName;
+
+static inline void FcCharSetDestroy(FcCharSet*) {}
+static inline void FcFontSetDestroy(FcFontSet*) {}
+static inline void FcLangSetDestroy(FcLangSet*) {}
+static inline void FcObjectSetDestroy(FcObjectSet*) {}
+static inline void FcPatternDestroy(FcPattern*) {}
+static inline void FcConfigDestroy(FcConfig*) {}
