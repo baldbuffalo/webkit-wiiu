75fad5e2834f1a06dec128b4d73cea4ddbb3bc61
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-03-17 00:08:48 +0400

    Update wkcmedia.h

--- Source/WebKit/WKC/include/wkc/wkcmedia.h
+++ Source/WebKit/WKC/include/wkc/wkcmedia.h
@@ -1,8 +1,15 @@
 #pragma once
 #include <wkc/wkcbase.h>
 
-// WKC media type definitions stub
 enum {
     WKC_MEDIA_UISTRING_BROADCAST = 0,
     WKC_MEDIA_UISTRING_LOADING   = 1,
 };
+
+#ifdef __cplusplus
+struct WKCMediaPlayerProcs;
+struct WKCMediaPlayerCallbacks;
+#else
+typedef struct WKCMediaPlayerProcs WKCMediaPlayerProcs;
+typedef struct WKCMediaPlayerCallbacks WKCMediaPlayerCallbacks;
+#endif
