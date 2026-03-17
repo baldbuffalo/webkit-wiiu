613164d218886fea96901ccf007b0a2c2528e754
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-03-17 06:46:22 +0400

    Update generate-settings.py

--- Source/WebKit/WKC/scripts/generate-settings.py
+++ Source/WebKit/WKC/scripts/generate-settings.py
@@ -1,4 +1,4 @@
-import sys, re
+import sys
 
 yaml_file = sys.argv[1]
 out_file = sys.argv[2]
@@ -7,8 +7,12 @@ with open(out_file, 'w') as f:
     f.write('#pragma once\n')
     f.write('#include <wtf/text/WTFString.h>\n')
     f.write('namespace WebCore {\n')
+    f.write('struct SettingsValues { };\n')
     f.write('class Settings {\n')
     f.write('public:\n')
     f.write('    static Settings& shared();\n')
+    f.write('    const SettingsValues& values() const { return m_values; }\n')
+    f.write('private:\n')
+    f.write('    SettingsValues m_values;\n')
     f.write('};\n')
     f.write('} // namespace WebCore\n')
