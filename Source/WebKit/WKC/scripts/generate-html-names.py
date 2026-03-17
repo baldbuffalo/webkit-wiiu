50414801b948350dacb64981c0819c0e922b407c
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-03-17 05:11:52 +0400

    Create generate-html-names.py

---
+++ Source/WebKit/WKC/scripts/generate-html-names.py
@@ -0,0 +1,37 @@
+import os, sys
+
+tags_file = sys.argv[1]
+attrs_file = sys.argv[2]
+out_file = sys.argv[3]
+
+def read_names(path):
+    names = []
+    try:
+        with open(path) as f:
+            for line in f:
+                line = line.strip()
+                if not line or line.startswith('#'):
+                    continue
+                name = line.split()[0].split(',')[0].strip()
+                if name:
+                    names.append(name)
+    except FileNotFoundError:
+        pass
+    return names
+
+tags = read_names(tags_file)
+attrs = read_names(attrs_file)
+
+with open(out_file, 'w') as f:
+    f.write('#pragma once\n')
+    f.write('#include <wtf/text/AtomString.h>\n')
+    f.write('namespace WebCore {\n')
+    f.write('class QualifiedName;\n')
+    f.write('namespace HTMLNames {\n')
+    for tag in tags:
+        f.write(f'extern const WTF::AtomString& {tag}Tag_localName;\n')
+    for attr in attrs:
+        f.write(f'extern const WTF::AtomString& {attr}Attr_localName;\n')
+    f.write('void init();\n')
+    f.write('} // namespace HTMLNames\n')
+    f.write('} // namespace WebCore\n')
