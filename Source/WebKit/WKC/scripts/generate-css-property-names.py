dbd4d2940701e44b41a12a1c43bf2deafbc8dfd0
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-03-17 22:51:09 +0400

    Update generate-css-property-names.py

--- Source/WebKit/WKC/scripts/generate-css-property-names.py
+++ Source/WebKit/WKC/scripts/generate-css-property-names.py
@@ -3,7 +3,6 @@ import sys, re, os, glob
 out_file = sys.argv[1]
 source_dir = sys.argv[2]
 
-# Scan all .h and .cpp files for CSSProperty identifiers
 identifiers = set()
 pattern = re.compile(r'\bCSSProperty([A-Z][a-zA-Z0-9]*)\b')
 
@@ -16,16 +15,18 @@ for ext in ('*.h', '*.cpp'):
         except Exception:
             pass
 
-# Sort for stability
 props = sorted(identifiers)
 
 with open(out_file, 'w') as f:
     f.write('#pragma once\n')
+    f.write('#include <cstdint>\n')
     f.write('namespace WebCore {\n')
-    f.write('enum CSSPropertyID {\n')
+    f.write('enum CSSPropertyID : uint16_t {\n')
     f.write('    CSSPropertyInvalid = 0,\n')
     for i, name in enumerate(props, 1):
         f.write(f'    {name} = {i},\n')
+    f.write(f'    cssPropertyIDEnumValueCount = {len(props) + 1},\n')
     f.write('};\n')
     f.write('} // namespace WebCore\n')
     f.write('using WebCore::CSSPropertyID;\n')
+    f.write('using WebCore::cssPropertyIDEnumValueCount;\n')
