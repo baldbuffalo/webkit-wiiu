6248dab6d8cf5bb3af1074cad03268316b476cc8
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-03-18 02:56:32 +0400

    Update RunLoopWKC.cpp

--- Source/WebCore/platform/WKC/RunLoopWKC.cpp
+++ Source/WebCore/platform/WKC/RunLoopWKC.cpp
@@ -17,10 +17,16 @@ void RunLoop::run()
 
 void RunLoop::stop()
 {
+    // Locker locker { m_loopLock };
+    // m_shutdown = true;
+    // wakeUpWithLock();
 }
 
 void RunLoop::wakeUp()
 {
+    // Bare metal — no cross-thread wakeup needed.
+    // Locker locker { m_loopLock };
+    // wakeUpWithLock();
 }
 
 RunLoop::CycleResult RunLoop::cycle(RunLoopMode)
