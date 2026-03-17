ea0b743c546294c441fcaffbebd57340140ff20d
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-03-17 05:01:55 +0400

    Update PopupMenuWKC.h

--- Source/WebCore/platform/WKC/PopupMenuWKC.h
+++ Source/WebCore/platform/WKC/PopupMenuWKC.h
@@ -5,7 +5,7 @@
 
 namespace WebCore {
 
-class LocalFrameView;
+class FrameView;
 class PopupMenuClient;
 
 class PopupMenuWKC : public PopupMenu {
@@ -13,7 +13,7 @@ public:
     PopupMenuWKC(PopupMenuClient*);
     ~PopupMenuWKC() override;
 
-    void show(const IntRect&, LocalFrameView*, int index) override;
+    void show(const IntRect&, FrameView*, int index) override;
     void hide() override;
     void updateFromElement() override;
     void disconnectClient() override;
