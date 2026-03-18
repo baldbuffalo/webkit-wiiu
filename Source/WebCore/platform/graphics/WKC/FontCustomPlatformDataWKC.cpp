8bbcdcbcd43e9aa25908f7ab0a8c865fa84ad6cf
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-03-19 00:47:08 +0400

    Update FontCustomPlatformDataWKC.cpp

--- Source/WebCore/platform/graphics/WKC/FontCustomPlatformDataWKC.cpp
+++ Source/WebCore/platform/graphics/WKC/FontCustomPlatformDataWKC.cpp
@@ -15,123 +15,72 @@
  * along with this library; see the file COPYING.LIB.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA 02110-1301, USA.
- *
  */
 
 #include "config.h"
 #include "FontCustomPlatformData.h"
 #include "FontPlatformDataWKC.h"
 
-#include "CString.h"
 #include "FontDescription.h"
+#include "FontPlatformData.h"
 #include "SharedBuffer.h"
-#include "WOFFFileFormat.h"
 
+#include <wtf/text/CString.h>
 #include <wkc/wkcpeer.h>
 #include <wkc/wkcgpeer.h>
 
 namespace WebCore {
 
-FontCustomPlatformData::FontCustomPlatformData(SharedBuffer* buf)
-    : m_buffer(buf)
-    , m_registeredId(0)
+FontCustomPlatformData::FontCustomPlatformData(SharedBuffer& buffer)
+    : m_buffer(buffer)
+    , m_registeredId(-1)
 {
     ::memset(m_familyName, 0, sizeof(m_familyName));
-    m_buffer->ref();
 }
 
 FontCustomPlatformData::~FontCustomPlatformData()
 {
-    if (m_registeredId>=0)
+    if (m_registeredId >= 0)
         wkcFontEngineUnregisterFontPeer(m_registeredId);
-    if (m_buffer)
-        m_buffer->deref();
 }
 
-FontCustomPlatformData*
-FontCustomPlatformData::create(SharedBuffer* buf)
+std::unique_ptr<FontCustomPlatformData> FontCustomPlatformData::create(SharedBuffer& buffer, String&)
 {
-    FontCustomPlatformData* self = 0;
-    self = new FontCustomPlatformData(buf);
-    if (!self->construct()) {
-        delete self;
-        return 0;
-    }
+    auto self = std::unique_ptr<FontCustomPlatformData>(new FontCustomPlatformData(buffer));
+    if (!self->construct())
+        return nullptr;
     return self;
 }
 
-bool
-FontCustomPlatformData::construct()
+bool FontCustomPlatformData::construct()
 {
-    m_registeredId = wkcFontEngineRegisterFontPeer(WKC_FONT_ENGINE_REGISTER_TYPE_MEMORY, reinterpret_cast<const unsigned char *>(m_buffer->data()), m_buffer->size());
-    if (m_registeredId<0)
+    auto contiguous = m_buffer->makeContiguous();
+    const auto* data = contiguous->data();
+    size_t size = contiguous->size();
+
+    m_registeredId = wkcFontEngineRegisterFontPeer(
+        WKC_FONT_ENGINE_REGISTER_TYPE_MEMORY,
+        reinterpret_cast<const unsigned char*>(data),
+        size);
+    if (m_registeredId < 0)
         return false;
 
-    bool ret = wkcFontEngineGetFamilyNamePeer(m_registeredId, m_familyName, sizeof(m_familyName));
-    if (!ret)
-        return false;
-
-    return true;
+    return wkcFontEngineGetFamilyNamePeer(m_registeredId, m_familyName, sizeof(m_familyName));
 }
 
-FontCustomPlatformData*
-createFontCustomPlatformData(SharedBuffer* buffer)
+FontPlatformData FontCustomPlatformData::fontPlatformData(const FontDescription& desc, bool bold, bool italic, float size)
 {
-    if (!buffer)
-        return 0;
-
-    RefPtr<SharedBuffer> sfntBuffer;
-    if (isWOFF(buffer)) {
-        Vector<char> sfnt;
-        if (!convertWOFFToSfnt(buffer, sfnt))
-            return 0;
-
-        sfntBuffer = SharedBuffer::adoptVector(sfnt);
-        buffer = sfntBuffer.get();
-    }
-
-    const unsigned char* data = reinterpret_cast<const unsigned char *>(buffer->data());
-    const unsigned int len = buffer->size();
-    if (!data || !len)
-        return 0;
-
-    if (!wkcFontEngineCanSupportPeer(data, len))
-        return 0;
-
-    FontCustomPlatformData* self = 0;
-    self = FontCustomPlatformData::create(buffer);
-    return self;
-}
-
-FontPlatformData
-FontCustomPlatformData::fontPlatformData(int size, bool bold, bool italic, FontOrientation orientation, TextOrientation textOrientation, FontWidthVariant widthVariant, FontRenderingMode mode)
-{
-    FontDescription desc;
-
-    desc.setComputedSize(size);
-    desc.setWeight(bold ? FontWeightBold : FontWeightNormal);
-    desc.setItalic(italic);
-    desc.setRenderingMode(mode);
-    desc.setOrientation(orientation);
-    desc.setTextOrientation(textOrientation);
-
-    FontPlatformData fd(desc, m_familyName);
+    FontPlatformData fd(desc, AtomString::fromUTF8(m_familyName));
     if (fd.font() && fd.font()->font())
         return fd;
-    else
-        return FontPlatformData(desc, "systemfont");
+    return FontPlatformData(desc, "systemfont"_s);
 }
 
-bool
-FontCustomPlatformData::supportsFormat(const WTF::String& format)
+bool FontCustomPlatformData::supportsFormat(const String& format)
 {
-#if ENABLE(SVG_FONTS)
-    if (format=="svg" || format=="svgz")
-        return true;
-#endif
-
-    return wkcFontEngineCanSupportByFormatNamePeer(reinterpret_cast<const unsigned char*>(format.utf8().data()), format.utf8().length());
+    return wkcFontEngineCanSupportByFormatNamePeer(
+        reinterpret_cast<const unsigned char*>(format.utf8().data()),
+        format.utf8().length());
 }
 
-
-} // namespace
+} // namespace WebCore
