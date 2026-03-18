03ab10c45042622016cb38565f6e774ffbd3b67e
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-03-19 02:43:27 +0400

    Update FontPlatformData.h

--- Source/WebKit/WKC/stubs/WebCore/FontPlatformData.h
+++ Source/WebKit/WKC/stubs/WebCore/FontPlatformData.h
@@ -1,9 +1,3 @@
 #pragma once
 #include <WebCore/FontPlatformSerializedData.h>
 #include_next <WebCore/FontPlatformData.h>
-
-namespace WKC { class WKCFontInfo; }
-namespace WebCore {
-    // WKC accessor — defined in FontPlatformDataWKC.cpp
-    // Declared here so WKC graphics code can call platformData.font()
-}
