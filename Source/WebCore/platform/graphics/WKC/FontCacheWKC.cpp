1fc06a4349eae865e64bcf390e4ca3196674ccf6
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-03-20 02:32:34 +0400

    Update FontCacheWKC.cpp

--- Source/WebCore/platform/graphics/WKC/FontCacheWKC.cpp
+++ Source/WebCore/platform/graphics/WKC/FontCacheWKC.cpp
@@ -54,7 +54,8 @@ std::unique_ptr<FontPlatformData> FontCache::createFontPlatformData(const FontDe
     else if (w < 850) wkcWeight = WKC_FONT_WEIGHT_800;
     else              wkcWeight = WKC_FONT_WEIGHT_900;
 
-    bool italic = isItalic(desc.italic());
+    auto slope = desc.fontSelectionRequest().slope;
+    bool italic = slope && slope.value() >= italicThreshold();
     bool horizontal = desc.orientation() == FontOrientation::Horizontal;
 
     auto familyUTF8 = family.string().utf8();
