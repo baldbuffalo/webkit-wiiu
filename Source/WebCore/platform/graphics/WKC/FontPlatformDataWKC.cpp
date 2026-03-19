421645a3bb38e2e88ac90193c0d22eea5a365e33
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-03-19 23:47:43 +0400

    Update FontPlatformDataWKC.cpp

--- Source/WebCore/platform/graphics/WKC/FontPlatformDataWKC.cpp
+++ Source/WebCore/platform/graphics/WKC/FontPlatformDataWKC.cpp
@@ -1,54 +1,155 @@
-#pragma once
+/*
+ * Copyright (C) 2007 Kevin Ollivier <kevino@theolliviers.com>
+ * Copyright (c) 2010-2014 ACCESS CO., LTD. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE COMPUTER, INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE COMPUTER, INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
-#include <wtf/text/CString.h>
+#include "config.h"
+#include "FontPlatformData.h"
+#include "FontPlatformDataWKC.h"
+
+#include <wtf/HashMap.h>
+#include <wtf/NeverDestroyed.h>
+#include <wkc/wkcpeer.h>
+#include <wkc/wkcgpeer.h>
+
+static bool gEnableScalingMonosizeFont = false;
+
+namespace WebCore {
+
+void FontPlatformData_enableScalingMonosizeFont(bool flag)
+{
+    gEnableScalingMonosizeFont = flag;
+}
+
+// ─── WKC font registry keyed by FontPlatformData hash ───────────────────────
+
+static HashMap<unsigned, WKC::WKCFontInfo*>& wkcFontMap()
+{
+    static NeverDestroyed<HashMap<unsigned, WKC::WKCFontInfo*>> map;
+    return map;
+}
+
+WKC::WKCFontInfo* wkcGetFont(const FontPlatformData& d)
+{
+    auto it = wkcFontMap().find(d.hash());
+    if (it != wkcFontMap().end())
+        return it->value;
+    return nullptr;
+}
+
+void wkcRegisterFont(unsigned hash, WKC::WKCFontInfo* info)
+{
+    wkcFontMap().set(hash, info);
+}
+
+void wkcUnregisterFont(unsigned hash)
+{
+    auto it = wkcFontMap().find(hash);
+    if (it != wkcFontMap().end()) {
+        delete it->value;
+        wkcFontMap().remove(it);
+    }
+}
+
+} // namespace WebCore
+
+// ─── WKCFontInfo implementation ─────────────────────────────────────────────
 
 namespace WKC {
 
-class WKCFontInfo {
-public:
-    ~WKCFontInfo();
-    static WKCFontInfo* create(float size, int weight, bool italic, bool horizontal, bool verticalright, int family, const char* familyName);
-    static WKCFontInfo* copy(const WKCFontInfo* other);
-    bool isEqual(const WKCFontInfo* other);
-    const WTF::CString& familyName() const { return m_familyName; }
-    void* font() const { return m_font; }
-    float scale() const { return m_scale; }
-    float iscale() const { return m_iscale; }
-    float requestSize() const { return m_requestSize; }
-    float createdSize() const { return m_createdSize; }
-    int weight() const { return m_weight; }
-    bool isItalic() const { return m_isItalic; }
-    bool canScale() const { return m_canScale; }
-    float ascent() const { return m_ascent; }
-    float descent() const { return m_descent; }
-    float lineSpacing() const { return m_lineSpacing; }
-    void setSpecificUnicodeChar(int ch) { m_unicodeChar = ch; }
-    int specificUnicodeChar() const { return m_unicodeChar; }
-    bool horizontal() const { return m_horizontal; }
-private:
-    WKCFontInfo(const char* familyName);
-    bool construct(float size, int weight, bool italic, bool horizontal, bool verticalright, int family);
-    void* m_font { nullptr };
-    float m_scale { 1.f };
-    float m_iscale { 1.f };
-    float m_requestSize { 0.f };
-    float m_createdSize { 0.f };
-    int m_weight { 0 };
-    bool m_isItalic { false };
-    bool m_canScale { false };
-    float m_ascent { 0.f };
-    float m_descent { 0.f };
-    float m_lineSpacing { 0.f };
-    int m_unicodeChar { 0 };
-    bool m_horizontal { true };
-    WTF::CString m_familyName;
-};
+WKCFontInfo::WKCFontInfo(const char* familyName)
+    : m_familyName(familyName)
+{
+}
+
+WKCFontInfo::~WKCFontInfo()
+{
+    if (m_font)
+        wkcFontDeletePeer(m_font);
+}
+
+WKCFontInfo* WKCFontInfo::create(float size, int weight, bool italic, bool horizontal, bool verticalright, int family, const char* familyName)
+{
+    WKCFontInfo* self = new WKCFontInfo(familyName);
+    if (!self->construct(size, weight, italic, horizontal, verticalright, family)) {
+        delete self;
+        return nullptr;
+    }
+    return self;
+}
+
+bool WKCFontInfo::construct(float size, int weight, bool italic, bool horizontal, bool verticalright, int family)
+{
+    m_font = wkcFontNewPeer((int)size, weight, italic, horizontal, verticalright, family, m_familyName.data());
+    m_scale = 1.f;
+    m_requestSize = size;
+    m_createdSize = m_font ? wkcFontGetSizePeer(m_font) : m_requestSize;
+    if (gEnableScalingMonosizeFont && m_createdSize != m_requestSize)
+        m_scale = m_requestSize / m_createdSize;
+    m_iscale = 1.f / m_scale;
+    m_weight = weight;
+    m_isItalic = italic;
+    if (m_font) {
+        m_canScale = wkcFontCanScalePeer(m_font);
+        m_ascent = wkcFontGetAscentPeer(m_font);
+        m_descent = wkcFontGetDescentPeer(m_font);
+        m_lineSpacing = wkcFontGetLineSpacingPeer(m_font);
+    }
+    m_unicodeChar = 0;
+    m_horizontal = horizontal;
+    return true;
+}
+
+WKCFontInfo* WKCFontInfo::copy(const WKCFontInfo* other)
+{
+    WKCFontInfo* self = new WKCFontInfo(other->m_familyName.data());
+    self->m_font       = other->m_font ? wkcFontNewCopyPeer(other->m_font) : nullptr;
+    self->m_scale      = other->m_scale;
+    self->m_iscale     = other->m_iscale;
+    self->m_requestSize= other->m_requestSize;
+    self->m_createdSize= other->m_createdSize;
+    self->m_weight     = other->m_weight;
+    self->m_isItalic   = other->m_isItalic;
+    self->m_canScale   = other->m_canScale;
+    self->m_ascent     = other->m_ascent;
+    self->m_descent    = other->m_descent;
+    self->m_lineSpacing= other->m_lineSpacing;
+    self->m_unicodeChar= other->m_unicodeChar;
+    self->m_horizontal = other->m_horizontal;
+    return self;
+}
+
+bool WKCFontInfo::isEqual(const WKCFontInfo* other)
+{
+    if (m_scale != other->m_scale ||
+        m_requestSize != other->m_requestSize ||
+        m_createdSize != other->m_createdSize ||
+        m_weight != other->m_weight ||
+        m_isItalic != other->m_isItalic ||
+        m_unicodeChar != other->m_unicodeChar)
+        return false;
+    return wkcFontIsEqualPeer(m_font, other->m_font);
+}
 
 } // namespace WKC
-
-namespace WebCore {
-class FontPlatformData;
-WKC::WKCFontInfo* wkcGetFont(const FontPlatformData&);
-void wkcRegisterFont(unsigned hash, WKC::WKCFontInfo*);
-void wkcUnregisterFont(unsigned hash);
-} // namespace WebCore
