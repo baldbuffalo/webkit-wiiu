ca97240e38c05b9a31a605443797c0d1a1eca1a4
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-03-19 23:48:01 +0400

    Update FontCacheWKC.cpp

--- Source/WebCore/platform/graphics/WKC/FontCacheWKC.cpp
+++ Source/WebCore/platform/graphics/WKC/FontCacheWKC.cpp
@@ -4,8 +4,11 @@
 #include "Font.h"
 #include "FontDescription.h"
 #include "FontPlatformData.h"
+#include "FontPlatformDataWKC.h"
 #include "FontCreationContext.h"
 #include <wtf/text/AtomString.h>
+#include <wkc/wkcpeer.h>
+#include <wkc/wkcgpeer.h>
 
 namespace WebCore {
 
@@ -28,16 +31,52 @@ ASCIILiteral FontCache::platformAlternateFamilyName(const String&)
     return { };
 }
 
-std::unique_ptr<FontPlatformData> FontCache::createFontPlatformData(const FontDescription& fontDescription, const AtomString&, const FontCreationContext&, OptionSet<FontLookupOptions>)
+std::unique_ptr<FontPlatformData> FontCache::createFontPlatformData(const FontDescription& desc, const AtomString& family, const FontCreationContext&, OptionSet<FontLookupOptions>)
 {
-    return makeUnique<FontPlatformData>(
-        fontDescription.computedSize(),
+    auto pd = makeUnique<FontPlatformData>(
+        desc.computedSize(),
         false,
         false,
-        fontDescription.orientation(),
-        fontDescription.widthVariant(),
-        fontDescription.textRenderingMode()
+        desc.orientation(),
+        desc.widthVariant(),
+        desc.textRenderingMode()
     );
+
+    // Map FontDescription generic family to WKC family constant
+    int wkcFamily = WKC_FONT_FAMILY_NONE;
+    switch (desc.genericFamily()) {
+    case FontDescription::GenericFamilyType::Serif:      wkcFamily = WKC_FONT_FAMILY_SERIF;      break;
+    case FontDescription::GenericFamilyType::SansSerif:  wkcFamily = WKC_FONT_FAMILY_SANSSERIF;  break;
+    case FontDescription::GenericFamilyType::Monospace:  wkcFamily = WKC_FONT_FAMILY_MONOSPACE;  break;
+    case FontDescription::GenericFamilyType::Cursive:    wkcFamily = WKC_FONT_FAMILY_CURSIVE;    break;
+    case FontDescription::GenericFamilyType::Fantasy:    wkcFamily = WKC_FONT_FAMILY_FANTASY;    break;
+    default: break;
+    }
+
+    int w = (int)desc.weight();
+    int wkcWeight = WKC_FONT_WEIGHT_NORMAL;
+    if      (w < 150) wkcWeight = WKC_FONT_WEIGHT_100;
+    else if (w < 250) wkcWeight = WKC_FONT_WEIGHT_200;
+    else if (w < 350) wkcWeight = WKC_FONT_WEIGHT_300;
+    else if (w < 450) wkcWeight = WKC_FONT_WEIGHT_400;
+    else if (w < 550) wkcWeight = WKC_FONT_WEIGHT_500;
+    else if (w < 650) wkcWeight = WKC_FONT_WEIGHT_600;
+    else if (w < 750) wkcWeight = WKC_FONT_WEIGHT_700;
+    else if (w < 850) wkcWeight = WKC_FONT_WEIGHT_800;
+    else              wkcWeight = WKC_FONT_WEIGHT_900;
+
+    bool italic = desc.italic() == FontSelectionValue(1);
+    bool horizontal = desc.orientation() == FontOrientation::Horizontal;
+
+    auto familyUTF8 = family.string().utf8();
+    WKC::WKCFontInfo* info = WKC::WKCFontInfo::create(
+        desc.computedSize(), wkcWeight, italic, horizontal, false,
+        wkcFamily, familyUTF8.data());
+
+    if (info)
+        wkcRegisterFont(pd->hash(), info);
+
+    return pd;
 }
 
 Ref<Font> FontCache::lastResortFallbackFont(const FontDescription& fontDescription)
