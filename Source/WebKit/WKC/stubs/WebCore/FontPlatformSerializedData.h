92397357a21d3940686115cc7b07a1a55d7c5753
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-03-19 21:08:08 +0400

    Update FontPlatformSerializedData.h

--- Source/WebKit/WKC/stubs/WebCore/FontPlatformSerializedData.h
+++ Source/WebKit/WKC/stubs/WebCore/FontPlatformSerializedData.h
@@ -1,2 +1,14 @@
 #pragma once
+
+#include <wtf/Forward.h>
+#include <wtf/RefPtr.h>
+#include <wtf/text/WTFString.h>
+
+namespace WebCore { class SharedBuffer; }
+
 struct FontPlatformSerializedData { };
+
+struct CustomFontCreationData {
+    WTF::RefPtr<WebCore::SharedBuffer> fontFaceData;
+    WTF::String itemInCollection;
+};
