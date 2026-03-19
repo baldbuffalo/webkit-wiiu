8d79b4a200e62029a8c4daa75382983aba1f820b
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-03-19 23:02:27 +0400

    Update patch-font-custom-platform-data.py

--- Source/WebKit/WKC/scripts/patch-font-custom-platform-data.py
+++ Source/WebKit/WKC/scripts/patch-font-custom-platform-data.py
@@ -7,8 +7,11 @@ with open(path, 'r') as f:
 insertion = "#elif WTF_PLATFORM_WKC\n    FontCustomPlatformData();\n"
 result = []
 done = False
+in_struct = False
 for line in lines:
-    if not done and line.strip() == "#elif USE(SKIA)":
+    if 'struct FontCustomPlatformData' in line:
+        in_struct = True
+    if not done and in_struct and line.strip() == "#elif USE(SKIA)":
         result.append(insertion)
         done = True
     result.append(line)
