d101db4d765acccf9ae8cd7ccacf37fd2f9b8d6b
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-03-20 07:56:59 +0400

    Update WKCForwardDecls.h

--- Source/WebKit/WKC/stubs/WKCForwardDecls.h
+++ Source/WebKit/WKC/stubs/WKCForwardDecls.h
@@ -1,5 +1,10 @@
 #pragma once
 
+// Must be defined before ICU headers are included via HTMLNames.h
+#ifndef U_SHOW_CPLUSPLUS_API
+#define U_SHOW_CPLUSPLUS_API 0
+#endif
+
 #include "HTMLNames.h"
 
 namespace WebCore {
