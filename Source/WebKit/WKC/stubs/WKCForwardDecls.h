9192030291d043ffb248d354445fc6d3c12972b6
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-04-01 02:53:30 +0400

    Update WKCForwardDecls.h

--- Source/WebKit/WKC/stubs/WKCForwardDecls.h
+++ Source/WebKit/WKC/stubs/WKCForwardDecls.h
@@ -1,9 +1,7 @@
 #pragma once
-
 #ifndef U_SHOW_CPLUSPLUS_API
 #define U_SHOW_CPLUSPLUS_API 0
 #endif
-
 #include "QualifiedName.h"
 
 namespace WebCore {
@@ -15,5 +13,16 @@ public:
 
 class HTMLSelectedContentElement;
 class HTMLOptGroupElement;
+class HTMLMeterElement;
 
 } // namespace WebCore
+
+namespace WTF {
+
+template<> struct TypeCastTraits<const WebCore::HTMLMeterElement, const WebCore::Element, false> {
+    static bool isOfType(const WebCore::Element&);
+};
+template<> struct TypeCastTraits<WebCore::HTMLMeterElement, WebCore::Element, false>
+    : TypeCastTraits<const WebCore::HTMLMeterElement, const WebCore::Element, false> { };
+
+} // namespace WTF
