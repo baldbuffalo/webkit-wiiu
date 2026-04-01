7419e04afe96ae36ee19cd8a3b718a613e686229
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-04-01 11:11:03 +0400

    Update RenderThemeWKC.cpp

--- Source/WebCore/platform/WKC/RenderThemeWKC.cpp
+++ Source/WebCore/platform/WKC/RenderThemeWKC.cpp
@@ -40,6 +40,10 @@
 #include "RenderMeter.h"
 #include "RenderProgress.h"
 #include "RenderTheme.h"
+#include "RenderTheme.h"
+#include "RenderElementInlines.h"
+#include "RenderObjectStyle.h"
+#include "RenderStyleInlines.h"
 #include "StringTruncator.h"
 #include "TimeRanges.h"
 #include <wtf/text/StringBuilder.h>
