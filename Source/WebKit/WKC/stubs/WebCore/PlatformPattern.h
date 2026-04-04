76ce3a498e8e63b6a0a0ac083bd96d9490dde6b4
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-04-05 00:49:27 +0400

    Update PlatformPattern.h

--- Source/WebKit/WKC/stubs/WebCore/PlatformPattern.h
+++ Source/WebKit/WKC/stubs/WebCore/PlatformPattern.h
@@ -1,7 +1,9 @@
 #pragma once
 
+// Pull in the real PlatformPattern.h from platform/graphics/
 #include_next <WebCore/PlatformPattern.h>
 
+// Add WKC platform pattern type
 namespace WebCore {
 class PlatformPatternWKC;
 using PlatformPatternPtr = PlatformPatternWKC*;
