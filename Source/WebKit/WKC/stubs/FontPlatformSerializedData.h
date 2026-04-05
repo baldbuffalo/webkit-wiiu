40a1454cb88a7b5ae79f6804ffbcd5b02fea208d
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-04-05 23:45:39 +0400

    Create FontPlatformSerializedData.h

---
+++ Source/WebKit/WKC/stubs/FontPlatformSerializedData.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// WKC stub for FontPlatformSerializedData
+// Font serialization is not needed on bare metal WKC.
+namespace WebCore {
+
+struct FontPlatformSerializedData {
+};
+
+} // namespace WebCore
