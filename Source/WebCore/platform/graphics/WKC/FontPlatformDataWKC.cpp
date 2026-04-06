ee9f39078b82abb3d61c84aa46e3e23582f5590c
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-04-06 11:30:52 +0400

    Update FontPlatformDataWKC.cpp

--- Source/WebCore/platform/graphics/WKC/FontPlatformDataWKC.cpp
+++ Source/WebCore/platform/graphics/WKC/FontPlatformDataWKC.cpp
@@ -25,7 +25,7 @@
  */
 
 #include "config.h"
-#include "FontPlatformSerializedData.h"
+#include "WebCore/FontPlatformSerializedData.h"
 #include "FontPlatformData.h"
 #include "FontPlatformDataWKC.h"
 
