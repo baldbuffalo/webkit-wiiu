5869e8b4d10be8113ac43827230272d0369899d6
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-04-06 12:38:43 +0400

    Create FontCustomPlatformData.h

---
+++ Source/WebKit/WKC/stubs/WebCore/FontCustomPlatformData.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <WebCore/FontPlatformData.h>
+#include <WebCore/RenderingResourceIdentifier.h>
+#include <wtf/Forward.h>
+#include <wtf/Noncopyable.h>
+#include <wtf/RefCounted.h>
+#include <wtf/TZoneMallocInlines.h>
+
+namespace WebCore {
+
+class SharedBuffer;
+class FontDescription;
+class FontCreationContext;
+enum class FontTechnology : uint8_t;
+
+template<typename> class FontTaggedSettings;
+using FontFeatureSettings = FontTaggedSettings<int>;
+
+struct FontCustomPlatformSerializedData {
+    Ref<SharedBuffer> fontFaceData;
+    String itemInCollection;
+    RenderingResourceIdentifier renderingResourceIdentifier;
+};
+
+struct FontCustomPlatformData : public RefCounted<FontCustomPlatformData> {
+    WTF_MAKE_TZONE_ALLOCATED_INLINE(FontCustomPlatformData);
+    WTF_MAKE_NONCOPYABLE(FontCustomPlatformData);
+public:
+    WEBCORE_EXPORT static RefPtr<FontCustomPlatformData> create(SharedBuffer&, const String&);
+    WEBCORE_EXPORT static RefPtr<FontCustomPlatformData> createMemorySafe(SharedBuffer&, const String&);
+
+    FontCustomPlatformData(FontPlatformData::CreationData&& data, RenderingResourceIdentifier identifier)
+        : creationData(WTFMove(data))
+        , m_renderingResourceIdentifier(identifier)
+    {
+    }
+
+    WEBCORE_EXPORT ~FontCustomPlatformData();
+
+    FontPlatformData fontPlatformData(const FontDescription&, bool bold, bool italic, const FontCreationContext&);
+
+    WEBCORE_EXPORT FontCustomPlatformSerializedData serializedData() const;
+    WEBCORE_EXPORT static std::optional<Ref<FontCustomPlatformData>> tryMakeFromSerializationData(FontCustomPlatformSerializedData&&, bool);
+
+    static bool supportsFormat(const String&);
+    static bool supportsTechnology(const FontTechnology&);
+
+    FontPlatformData::CreationData creationData;
+    RenderingResourceIdentifier m_renderingResourceIdentifier;
+};
+
+} // namespace WebCore
