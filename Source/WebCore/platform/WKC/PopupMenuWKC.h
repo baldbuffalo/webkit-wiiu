ecd6dc87b3988fbaa78920f0534616c52487b453
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-04-09 15:10:49 +0400

    Update PopupMenuWKC.h

--- Source/WebCore/platform/WKC/PopupMenuWKC.h
+++ Source/WebCore/platform/WKC/PopupMenuWKC.h
@@ -1,29 +1,22 @@
 #pragma once
-
+#include "IntRect.h"
 #include "PopupMenu.h"
 #include "helpers/privates/WKCPopupMenuClientPrivate.h"
-
 namespace WebCore {
-
 class LocalFrameView;
 class PopupMenuClient;
-
 class PopupMenuWKC : public PopupMenu {
 public:
     PopupMenuWKC(PopupMenuClient*);
     ~PopupMenuWKC() override;
-
     void show(const IntRect&, LocalFrameView&, int selectedIndex) override;
     void hide() override;
     void updateFromElement() override;
     void disconnectClient() override;
-
 private:
     PopupMenuClient* client() const { return m_popupClient; }
-
     PopupMenuClient* m_popupClient;
     WKC::PopupMenuClientPrivate* m_wkc;
     bool m_visible;
 };
-
 } // namespace WebCore
