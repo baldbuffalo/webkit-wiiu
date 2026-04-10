11554e580d5c4415db2cbc58a0bd5c2bfb6f38a2
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-04-10 09:03:41 +0400

    Update StyleTextEdge.h

--- Source/WebKit/WKC/stubs/WebCore/StyleTextEdge.h
+++ Source/WebKit/WKC/stubs/WebCore/StyleTextEdge.h
@@ -1,14 +1,18 @@
 #pragma once
 
 #include <stdint.h>
+#include <optional>
+#include <variant>
 
+// Minimal CSS keyword tags
 namespace CSS {
 namespace Keyword {
-struct Auto {};
+struct Auto  {};
 struct Leading {};
 } // namespace Keyword
 } // namespace CSS
 
+// TextEdgeType enum (used by rendering/style/StyleTextEdge.h)
 namespace WebCore {
 enum class TextEdgeType : uint8_t {
     Auto,
@@ -20,17 +24,67 @@ enum class TextEdgeType : uint8_t {
     Leading,
     Alphabetic
 };
-} // namespace WebCore
 
-namespace WebCore {
+enum class TextEdgeOver : uint8_t {
+    Text,
+    Cap,
+    Ex,
+    Ideographic,
+    IdeographicInk
+};
+
+enum class TextEdgeUnder : uint8_t {
+    Text,
+    Alphabetic,
+    Ideographic,
+    IdeographicInk
+};
+
+// Rendering-side TextEdge (rendering/style/StyleTextEdge.h)
+struct TextEdge {
+    TextEdgeType over  { TextEdgeType::Auto };
+    TextEdgeType under { TextEdgeType::Auto };
+    bool operator==(const TextEdge&) const = default;
+};
+
 namespace Style {
 
-template<typename DefaultKeyword>
-struct TextEdge {
-    WebCore::TextEdgeType over  { WebCore::TextEdgeType::Auto };
-    WebCore::TextEdgeType under { WebCore::TextEdgeType::Auto };
+struct TextEdgePair {
+    TextEdgeOver over;
+    TextEdgeUnder under;
+    bool operator==(const TextEdgePair&) const = default;
+};
 
-    bool operator==(const TextEdge&) const = default;
+// Style-side TextEdge template (style/values/inline/StyleTextEdge.h)
+template<typename K>
+struct TextEdge {
+    using Keyword = K;
+
+    TextEdge() : m_value(Keyword{}) { }
+    TextEdge(Keyword kw) : m_value(kw) { }
+    TextEdge(TextEdgeOver over, TextEdgeUnder under) : m_value(TextEdgePair{ over, under }) { }
+
+    bool isKeyword() const
+    {
+        return std::holds_alternative<Keyword>(m_value);
+    }
+
+    bool isTextEdgePair() const
+    {
+        return std::holds_alternative<TextEdgePair>(m_value);
+    }
+
+    std::optional<TextEdgePair> tryTextEdgePair() const
+    {
+        if (isTextEdgePair())
+            return std::get<TextEdgePair>(m_value);
+        return std::nullopt;
+    }
+
+    bool operator==(const TextEdge<K>&) const = default;
+
+protected:
+    std::variant<Keyword, TextEdgePair> m_value;
 };
 
 } // namespace Style
