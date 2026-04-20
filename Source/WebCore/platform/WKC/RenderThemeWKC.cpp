90ad0fa51e789cf6b7b530bb630af91829c0501a
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-04-20 06:45:19 +0400

    Update RenderThemeWKC.cpp

--- Source/WebCore/platform/WKC/RenderThemeWKC.cpp
+++ Source/WebCore/platform/WKC/RenderThemeWKC.cpp
@@ -63,14 +63,6 @@ static inline void WKCFloatRect_SetRect(WKCFloatRect* dst, const WKCFloatRect* s
 {
     *dst = *src;
 }
-static inline void WKCFloatSize_Set(WKCFloatSize* s, float w, float h)
-{
-    s->fWidth = w; s->fHeight = h;
-}
-static inline void WKCFloatPoint_Set(WKCFloatPoint* p, float x, float y)
-{
-    p->fX = x; p->fY = y;
-}
 
 namespace WebCore {
 
