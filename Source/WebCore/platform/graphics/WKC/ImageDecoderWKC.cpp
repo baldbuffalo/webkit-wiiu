7ea655cb6954b274e51e86988f0d91157ba581e8
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-04-21 22:03:10 +0400

    Delete Source/WebCore/platform/graphics/WKC/ImageDecoderWKC.cpp

--- Source/WebCore/platform/graphics/WKC/ImageDecoderWKC.cpp
+++
@@ -1,9 +0,0 @@
-/*
- *  Copyright (c) 2010-2014 ACCESS CO., LTD. All rights reserved.
- *
- *  [license omitted]
- */
-#include "config.h"
-// ImageFrame as a platform-implemented concrete class no longer exists in modern WebKit.
-// Image decoding is handled through BitmapImage / NativeImage instead.
-// This file is intentionally empty.
