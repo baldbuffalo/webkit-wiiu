ebd9d3065d35eec95367fe6e290f5b13b8015e15
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-05-05 16:01:41 +0400

    Add includes for WTF string types in WKCPlatformStubs.h

--- Source/WebKit/WKC/stubs/WKCPlatformStubs.h
+++ Source/WebKit/WKC/stubs/WKCPlatformStubs.h
@@ -1,6 +1,13 @@
 #pragma once
-
 #include <stdint.h>
+
+// WTF string types must be complete before <optional> is included,
+// otherwise GCC 16 SFINAE iterator probing triggers -Wsfinae-incomplete
+// warnings when these types are later defined.
+#include <wtf/text/WTFString.h>
+#include <wtf/text/CString.h>
+#include <wtf/text/AtomString.h>
+
 #include "WebCore/StyleTextEdge.h"
 #include "WebCore/PlatformPattern.h"
 #include "WebCore/PlatformPath.h"
