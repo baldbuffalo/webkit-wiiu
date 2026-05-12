8a1edc82d4390524045e0edaddd49d40e6d53cd3
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-05-12 13:47:13 +0400

    Update AuthenticationJarWKC.h

--- Source/WebCore/platform/network/WKC/AuthenticationJarWKC.h
+++ Source/WebCore/platform/network/WKC/AuthenticationJarWKC.h
@@ -1,34 +1,14 @@
 /*
  * Copyright (c) 2012,2013 ACCESS CO., LTD. All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY ACCESS CO.,LTD. ``AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL ACCESS CO.,LTD. OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
- * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ * [license unchanged]
  */
-#ifndef AuthenticationJar_h
-#define AuthenticationJar_h
+#pragma once
 
-#include "CString.h"
 #include <wtf/URL.h>
 #include <wtf/RefPtr.h>
-
+#include <wtf/text/WTFString.h>
+#include <wtf/Vector.h>
 #include "ResourceHandle.h"
 #include "ResourceHandleInternalWKC.h"
 
@@ -41,14 +21,12 @@ public:
 
     AuthenticationJar* get() { return this; }
 
-    bool Challenge(ResourceHandle *job, long httpCode);
-
-    bool getWebUserPassword(String url, ProtectionSpaceServerType& servertype, ProtectionSpaceAuthenticationScheme& authscheme, String realm, String& user, String& passwd);
-    void setWebUserPassword(String url, ProtectionSpaceServerType servertype, ProtectionSpaceAuthenticationScheme authscheme, String realm, String user, String passwd, String location = "", bool confirmed = false);
+    bool Challenge(ResourceHandle* job, long httpCode);
+    bool getWebUserPassword(String url, ProtectionSpaceServerType&, ProtectionSpaceAuthenticationScheme&, String realm, String& user, String& passwd);
+    void setWebUserPassword(String url, ProtectionSpaceServerType, ProtectionSpaceAuthenticationScheme, String realm, String user, String passwd, String location = String(), bool confirmed = false);
     void deleteWebUserPassword(String url, String realm);
-
-    bool getProxyUserPassword(String url, int port, ProtectionSpaceServerType& servertype, ProtectionSpaceAuthenticationScheme& authscheme, String& user, String& passwd);
-    void setProxyUserPassword(String url, int port, ProtectionSpaceServerType servertype, ProtectionSpaceAuthenticationScheme authscheme, String user, String passwd);
+    bool getProxyUserPassword(String url, int port, ProtectionSpaceServerType&, ProtectionSpaceAuthenticationScheme&, String& user, String& passwd);
+    void setProxyUserPassword(String url, int port, ProtectionSpaceServerType, ProtectionSpaceAuthenticationScheme, String user, String passwd);
     void deleteProxyUserPassword(String url, int port);
 
 private:
@@ -57,18 +35,10 @@ private:
         AuthInfo()
             : m_serverType(ProtectionSpaceServerHTTP)
             , m_authScheme(ProtectionSpaceAuthenticationSchemeDefault)
-            , m_host("")
             , m_port(0)
-            , m_basePath("")
-            , m_fullPath("")
-            , m_realm("")
             , m_confirmed(false)
-            , m_user("")
-            , m_passwd("")
-            , m_tmpUser("")
-            , m_tmpPasswd("")
-        {};
-        ~AuthInfo() {};
+        {}
+        ~AuthInfo() = default;
 
         ProtectionSpaceServerType           m_serverType;
         ProtectionSpaceAuthenticationScheme m_authScheme;
@@ -77,7 +47,6 @@ private:
         String m_basePath;
         String m_fullPath;
         String m_realm;
-
         bool   m_confirmed;
         String m_user;
         String m_passwd;
@@ -91,6 +60,4 @@ private:
     AuthInfo* longer(AuthInfo* current, AuthInfo* newinfo, bool compFull);
 };
 
-}
-
-#endif // AuthenticationJar_h
+} // namespace WebCore
